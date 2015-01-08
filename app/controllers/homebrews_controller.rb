89eaddc88259daac37912bf8fba8d171a2bfbc96
Helen Hood <helen.c.hood@gmail.com>
2015-01-08 16:09:46 -0500

    Add homebrew form, routes, & actions
    Switch this away from nested resources, don’t need them

--- app/controllers/homebrews_controller.rb
+++ app/controllers/homebrews_controller.rb
@@ -1,4 +1,21 @@
 class HomebrewsController < ApplicationController
   def index
   end
+  def new
+    @homebrew = Homebrew.new
+  end
+
+  def create
+    @homebrew = current_user.homebrews.build(homebrew_params)
+
+    if @homebrew.save
+      redirect_to @homebrew, notice: "Thanks! Your beer has been added."
+    end
+  end
+
+  private
+
+  def homebrew_params
+    params.require(:homebrew).permit(:name, :date_brewed, :description)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
 
   root "homebrews#index"
+
+  resources :users, only: [] do
+    resources :homebrews, only: [:new, :create]
+  end
 end
