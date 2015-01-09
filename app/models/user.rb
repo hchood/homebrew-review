0e7663736dc66560ee921fd946c28ed82b354c54
Helen Hood <helen.c.hood@gmail.com>
2015-01-09 14:30:45 -0500

    Add friendship associations on user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,19 @@ class User < ActiveRecord::Base
 
   paginates_per 20
 
-  has_many :homebrews, dependent: :destroy
+  has_many :homebrews,
+    dependent: :destroy
+  has_many :friendships,
+    dependent: :destroy
+  has_many :inverse_friendships,
+    class_name: "Friendship",
+    foreign_key: :friend_id,
+    dependent: :destroy
+  has_many :friends,
+    through: :friendships
+  has_many :inverse_friends,
+    through: :inverse_friendships,
+    source: :user
 
   validates :first_name, presence: true
   validates :last_name, presence: true
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,6 +4,10 @@ RSpec.describe User, :type => :model do
   let!(:user) { FactoryGirl.create(:user) }
 
   it { should have_many(:homebrews).dependent(:destroy) }
+  it { should have_many(:friendships).dependent(:destroy) }
+  it { should have_many(:inverse_friendships).dependent(:destroy) }
+  it { should have_many(:friends).through(:friendships) }
+  it { should have_many(:inverse_friends).through(:inverse_friendships) }
 
   it { should validate_presence_of :first_name }
   it { should validate_presence_of :last_name }
