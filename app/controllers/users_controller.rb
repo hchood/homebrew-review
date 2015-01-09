3548e31129beac51fc30e7f6cceed4925a264e3c
Helen Hood <helen.c.hood@gmail.com>
2015-01-09 13:34:50 -0500

    Add users show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,11 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
-  
+
   def index
     @users = User.order(:last_name, :first_name).page(params[:page])
+  end
+
+  def show
+    @user = User.find(params[:id])
   end
 end
