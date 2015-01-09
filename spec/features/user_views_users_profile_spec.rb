74228594082bd043c13601e88830dbc4c6703065
Helen Hood <helen.c.hood@gmail.com>
2015-01-09 15:47:25 -0500

    Update user profile spec to display friends

--- spec/features/user_views_users_profile_spec.rb
+++ spec/features/user_views_users_profile_spec.rb
@@ -7,10 +7,12 @@ feature "User views a user's profile", %Q{
   } do
 
   # Acceptance Criteria:
-  # - [ ] I must be authenticated to view a user's profile page
-  # - [ ] The user's first name and last name are displayed.
-  # - [ ] Links to each of the user's homebrews are displayed.
-  # - [ ] If the user has not added any homebrews yet, a message to that effect is displayed.
+  # - [x] I must be authenticated to view a user's profile page
+  # - [x] The user's first name and last name are displayed.
+  # - [x] Links to each of the user's homebrews are displayed.
+  # - [x] If the user has not added any homebrews yet, a message to that effect is displayed.
+  # - [ ] Links to each of the user's friends are displayed.
+  # - [ ] If the user doesn't have any friends yet, a message to that effect is displayed.
 
   context "authenticated user" do
     before :each do
@@ -18,25 +20,31 @@ feature "User views a user's profile", %Q{
       login_as(@user)
     end
 
-    scenario "user has added homebrews" do
+    scenario "user has added homebrews and has friends" do
       beers = FactoryGirl.create_list(:homebrew, 3, user: @user)
+      friendships = FactoryGirl.create_list(:friendship, 2, user: @user)
 
       visit user_path(@user)
 
       expect(page).to have_content @user.first_name
       expect(page).to have_content @user.last_name
+
+      @user.all_friends.each do |friend|
+        expect(page).to have_link friend.full_name
+      end
 
       beers.each do |beer|
         expect(page).to have_link beer.name
       end
     end
 
-    scenario "user hasn't added any homebrews" do
+    scenario "user hasn't added any homebrews or friends" do
       visit user_path(@user)
 
       expect(page).to have_content @user.first_name
       expect(page).to have_content @user.last_name
       expect(page).to have_content "#{@user.first_name} hasn't added any homebrews yet."
+      expect(page).to have_content "#{@user.first_name} hasn't made any friends yet."
     end
   end
 
