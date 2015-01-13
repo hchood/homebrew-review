b86d32a193102c5e50bd65c403d2421308607085
Helen Hood <helen.c.hood@gmail.com>
2015-01-13 17:44:16 -0500

    Wrap association & validation tests in describe blocks

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,23 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe User, :type => :model do
-  let!(:user) { FactoryGirl.create(:user) }
+  describe "associations" do
+    it { should have_many(:homebrews).dependent(:destroy) }
+    it { should have_many(:friendships).dependent(:destroy) }
+    it { should have_many(:inverse_friendships).dependent(:destroy) }
+    it { should have_many(:friends).through(:friendships) }
+    it { should have_many(:inverse_friends).through(:inverse_friendships) }
+    it { should have_many(:api_keys).dependent(:destroy) }
+  end
 
-  it { should have_many(:homebrews).dependent(:destroy) }
-  it { should have_many(:friendships).dependent(:destroy) }
-  it { should have_many(:inverse_friendships).dependent(:destroy) }
-  it { should have_many(:friends).through(:friendships) }
-  it { should have_many(:inverse_friends).through(:inverse_friendships) }
-  it { should have_many(:api_keys).dependent(:destroy) }
+  describe "validations" do
+    let!(:user) { FactoryGirl.create(:user) }
 
-  it { should validate_presence_of :first_name }
-  it { should validate_presence_of :last_name }
-  it { should validate_presence_of :email }
-  it { should validate_presence_of :password }
+    it { should validate_presence_of :first_name }
+    it { should validate_presence_of :last_name }
+    it { should validate_presence_of :email }
+    it { should validate_presence_of :password }
 
-  it { should validate_uniqueness_of :email}
+    it { should validate_uniqueness_of :email}
+  end
+
+
 
   describe "#all_friends" do
+    let!(:user) { FactoryGirl.create(:user) }
+    
     it "returns all a user's friends" do
       friendships = FactoryGirl.create_list(:friendship, 2, user: user)
       inverse_friendships = FactoryGirl.create_list(:friendship, 2, friend: user)
